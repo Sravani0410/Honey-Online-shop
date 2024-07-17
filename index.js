acb5fea8d1161264bb8cafcb06c63e15c030c6ea
sravani <sandhyapyla.8@gmail.com>
2024-07-17 12:22:48 +0530

    implement the read more in about us section

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutText = document.getElementById("about-text");
+    const moreBtn = document.getElementById("more-btn");
+  
+    moreBtn.addEventListener("click", function() {
+      if (aboutText.classList.contains("expanded")) {
+        aboutText.classList.remove("expanded");
+        aboutText.textContent = aboutText.dataset.fullText.substring(0, 350) + "...";
+        moreBtn.querySelector("span").textContent = "More";
+      } else {
+        aboutText.classList.add("expanded");
+        aboutText.textContent = aboutText.dataset.fullText;
+        moreBtn.querySelector("span").textContent = "Less";
+      }
+    });
+  });
+  
+//   document.addEventListener("DOMContentLoaded", function() {
+//     const beautyText = document.getElementById("beauty-text");
+//     const moreBtn = document.getElementById("more-btn");
+  
+//     moreBtn.addEventListener("click", function() {
+//       if (beautyText.classList.contains("expanded")) {
+//         beautyText.classList.remove("expanded");
+//         beautyText.textContent = beautyText.dataset.fullText.substring(0, 350) + "...";
+//         moreBtn.querySelector("span").textContent = "More";
+//       } else {
+//         beautyText.classList.add("expanded");
+//         beautyText.textContent = beautyText.dataset.fullText;
+//         moreBtn.querySelector("span").textContent = "Less";
+//       }
+//     });
+//   });
+  
